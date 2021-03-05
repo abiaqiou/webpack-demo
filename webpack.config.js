331954814cd9633538b84c062cc85b54b2d53404
abiaqiou <abiaqiou@gmail.com>
2021-03-05 14:42:38 +0800

    add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const { resolve } = require('path')
+
+module.exports = {
+  // 入口文件位置
+  entry: './src/index.js',
+
+  // 一个 chunk，一个 bundle
+  // entry: ['./src/index.js', './src/main.js']
+
+  // 多个 chunk，多个 bundle
+  // entry: {
+  //   bundle0: './src/index.js',
+  //   bundle1: './src/main.js',
+  // }
+
+  // 出口文件位置
+  output: {
+    filename: '[name].js',
+    path: resolve(__dirname, 'dist'),
+  },
+
+  // loader，让 Webpack 能够处理其它资源如 css img 等，Webpack 默认只能处理 js 和 json
+  module: {
+    rules: [],
+  },
+
+  // 插件，执行范围更广的任务
+  plugins: [],
+
+  // 打包模式，开发与生产这两种模式都会自动启用一些插件
+  mode: 'development',
+}
